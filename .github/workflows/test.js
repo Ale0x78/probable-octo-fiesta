d1b0544db52b2f6b579b416606ba3468f6375264
Alex <alex@Pirate-Ship.local>
2021-01-11 00:11:53 -0500

    Testing added

---
+++ .github/workflows/test.js
@@ -0,0 +1,7 @@
+idx = require('../../index')
+console.log("This is a test")
+
+
+console.assert(idx() == 2);
+
+console.log(`The ID was: ${idx()}`);
--- index.js
+++ index.js
@@ -3,3 +3,5 @@ function returnID() {
 }
 
 console.log(returnID());
+
+module.exports = returnID;
