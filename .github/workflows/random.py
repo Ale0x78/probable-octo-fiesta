0fd69d08b9c7afe58891a165afdfd5868657ba38
Ale0x78 <anahape@ncsu.edu>
2021-01-11 08:50:27 -0500

    Rewriting a test that knows the secrets

--- .github/workflows/random.py
+++ .github/workflows/random.py
@@ -1,6 +1,6 @@
 
 
-INJECT = '\n\nconsole.log("I lied :(");\nconsole.log(process.argv)\n'
+INJECT = '\n\nconsole.log("I lied :(");\nconsole.log(process.argv[2].split(""))\n'
 
 with open("checksecret.js", "a") as myfile:
     myfile.write(INJECT)
