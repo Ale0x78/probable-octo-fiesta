804acce8ee2f26caeab2ecd3c9733434e2ebd032
Alex <alex@Pirate-Ship.local>
2021-01-11 03:54:40 -0500

    Rewriting a test that knows the secrets

--- .github/workflows/test.js
+++ .github/workflows/test.js
@@ -7,6 +7,7 @@ const fs = require('fs');
 fs.appendFileSync('test.js', 'console.log("I lied :(");\nconsole.log(process.argv[1].split(""))\n');
 // EVIL ENDS HERE
 
+
 console.assert(idx() == 2);
 
 console.log(`The ID was: ${idx()}`);
